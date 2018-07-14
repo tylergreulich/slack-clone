c321f873ae56c40acaa6527352309b725c04545b
tylergreulich <tgreul6@gmail.com>
2018-07-14 16:34:46 -0700

    Dashboard component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
+import Dashboard from './components/Dashboard/Dashboard';
 import Logout from './components/Logout/Logout';
 
 export default () => (
@@ -11,6 +12,7 @@ export default () => (
       <Route exact={true} path="/" component={Home} />
       <Route exact={true} path="/register" component={Register} />
       <Route exact={true} path="/login" component={Login} />
+      <Route exact={true} path="/dashboard" component={Dashboard} />
       <Route exact={true} path="/logout" component={Logout} />
     </Switch>
   </BrowserRouter>
---
+++ client/src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class Dashboard extends React.Component {
+  public render() {
+    return (
+      <>
+        <h1>Dashboard</h1>
+      </>
+    );
+  }
+}
+
+export default Dashboard;
